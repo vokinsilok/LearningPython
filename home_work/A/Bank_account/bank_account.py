20e7cd4abfc873aa119f3604a7b823d019dad8b4
Huawei <yaroslav1234.1234567890@gmail.com>
2025-11-02 15:25:46 +1000

    Решил задачу с банком

---
+++ home_work/A/Bank_account/bank_account.py
@@ -0,0 +1,27 @@
+class BankAccount:
+    def __init__(self, balance: float=0.0):
+        self.__balance = balance
+
+    def add_balance(self, amount: float=0.0):
+        if amount < 0:
+            raise ValueError('amount cannot be negative')
+        self.__balance += amount
+
+    def less_balance(self, amount: float=0.0):
+        if amount < 0:
+            raise ValueError('amount cannot be negative')
+        if amount > self.__balance:
+            raise ValueError('amount cannot be greater than balance')
+        self.__balance -= amount
+
+    def get_balance(self):
+        return self.__balance
+
+    @property
+    def balance(self):
+        return self.__balance
+
+account_1 = BankAccount(100)
+account_1.less_balance(90)
+account_1.add_balance(50)
+print(account_1.get_balance())
